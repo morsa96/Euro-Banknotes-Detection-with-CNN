3d0414af2684361ceaea2fbd790170cdb34539f7
morsa96 <43095033+morsa96@users.noreply.github.com>
2023-08-09 23:07:17 +0200

    Add files via upload

--- Project.py
+++ Project.py
@@ -4,13 +4,14 @@ import os
 from matplotlib import pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def class_predicted(prediction):
     pred = np.argmax(prediction[0])
     return (CATEGORIES[pred])
 
 def prepare(img_path):
-    img = load_img(img_path, target_size=(250, 250))  # this is a PIL image
+    img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
     x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
     x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
     # Rescale by 1/255
@@ -42,8 +43,6 @@ def play_video(video_path):
         cv2.imshow('frame', frame)
         os.remove("frame%d.jpg"% count)
         count+=1
-        if count == 500:
-            break
         # Stop playing when entered 'q' from keyboard
         if cv2.waitKey(1) == ord('q'):
             break
@@ -54,48 +53,9 @@ position = (10,50)
     
 model = keras.models.load_model("euro-CNN.h5")
 CATEGORIES = ["5-euro","10-euro","20-euro","50-euro"]
-"""  
-cap = cv2.VideoCapture(0)
-# Getting original video params
-w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
 
-print("Width: " , w)
-print("Height: ", h)
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret or frame is None:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    #prediction = model.predict(prepare(frame))
-    cv2.putText(
-         frame, #numpy array on which text is written
-         CATEGORIES[int(prediction[0][0])], #text
-         position, #position at which writing has to start
-         cv2.FONT_HERSHEY_SIMPLEX, #font family
-         1, #font size
-         (209, 80, 0, 255), #font color
-         3) #font stroke
-    cv2.imwrite("frame%d.jpg"% count,frame)
-    #print(CATEGORIES[int(prediction[0][0])])
-    count +=1
-    
-cap.release()
-cv2.destroyAllWindows()
-
-prediction = model.predict(prepare('5euro.jpg'))
-print(class_predicted(prediction))
-prediction = model.predict(prepare('10euro.jpg'))
-print(class_predicted(prediction))
-prediction = model.predict(prepare('20euro.jpg'))
-print(class_predicted(prediction))
-prediction = model.predict(prepare('50euro.jpg'))
-print(class_predicted(prediction))
-"""
-
-play_video('1.mov')
+#test with a video
+play_video('prova.mov')
 
 
 
---
+++ model.py
@@ -0,0 +1,164 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import cv2 as cv
+import h5py
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+import tensorflow as tf
+
+base_dir = 'imgs'
+train_dir = os.path.join(base_dir,'training')
+validation_dir = os.path.join(base_dir,'validation')
+CATEGORIES = ["5-euro","10-euro","20-euro","50-euro"]
+
+train_5_dir = os.path.join(train_dir, '5-euro')
+train_10_dir = os.path.join(train_dir, '10-euro')
+train_20_dir = os.path.join(train_dir, '20-euro')
+train_50_dir = os.path.join(train_dir, '50-euro')
+val_5_dir = os.path.join(validation_dir, '5-euro')
+val_10_dir = os.path.join(validation_dir,'10-euro')
+val_20_dir = os.path.join(validation_dir, '20-euro')
+val_50_dir = os.path.join(validation_dir,'50-euro')
+
+print('total training 5 images:', len(os.listdir(train_5_dir)))
+print('total training 10 images:', len(os.listdir(train_10_dir)))
+print('total training 20 images:', len(os.listdir(train_20_dir)))
+print('total training 50 images:', len(os.listdir(train_50_dir)))
+print('total validation 5 images:', len(os.listdir(val_5_dir)))
+print('total validation 10 images:', len(os.listdir(val_10_dir)))
+print('total validation 20 images:', len(os.listdir(val_20_dir)))
+print('total validation 50 images:', len(os.listdir(val_50_dir)))
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=False) #we don't need to flip the images in our case.
+
+# Note that the validation data should not be augmented!
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 50 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        classes=['5-euro','10-euro','20-euro','50-euro'],
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=50,
+        # Since we use categorical_crossentropy loss, we need categorical labels
+        class_mode='categorical')
+
+# Flow validation images in batches of 25 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        classes=['5-euro','10-euro','20-euro','50-euro'],
+        target_size=(150, 150),
+        batch_size=25,
+        class_mode='categorical')
+
+num_classes= len(train_generator.class_indices)
+train_labels = train_generator.classes
+print(train_labels)
+# convert the training labels to categorical vectors 
+train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=num_classes)
+print("number of classes:",num_classes)
+print("number of labels:",train_labels)
+num_classes_val = len(validation_generator.class_indices)
+validation_labels = validation_generator.classes
+print(validation_labels)
+# convert the training labels to categorical vectors 
+validation_labels = tf.keras.utils.to_categorical(validation_labels, num_classes=num_classes_val)
+print("number of classes validation:",num_classes_val)
+print("number of labels validation:",validation_labels)
+
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+#Add a dropout rate of 0.5
+x = layers.Dropout(0.5)(x)
+
+# Create output layer with four nodes and softmax activation
+output = layers.Dense(num_classes, activation='softmax')(x)
+
+# Create model:
+# input = input feature map
+# output = input feature map + stacked convolution/maxpooling layers + fully 
+# connected layer + softmax output layer
+model = Model(img_input, output)
+
+# or categorical_crossentropy
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+history = model.fit(
+      train_generator,
+      steps_per_epoch=100,  # 5000 images = batch_size * steps
+      epochs=10,
+      validation_data=validation_generator,
+      validation_steps=80,  # 2000 images = batch_size * steps
+      verbose=2)
+
+model.save('euro-CNN.h5')
+
+# evaluating and printing results 
+score = model.evaluate(validation_generator, verbose = 0) 
+print('Test loss:', score[0]) 
+print('Test accuracy:', score[1])
+
+model.summary()
+
+# Retrieve a list of accuracy results on training and validation data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and validation data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.show()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+
+plt.show()
